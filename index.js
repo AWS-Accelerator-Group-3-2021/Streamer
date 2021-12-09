2dfc607184e5c625049c8059df38a7f80992554f
Prakhar Trivedi <prakh0706@outlook.com>
2021-12-09 23:15:01 +0530

    added attachments support

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ telegramBot.start((ctx) => {
 
 telegramBot.on('message', ctx => {
     if (!ctx.chat.title) return
-    if (ctx.chat.title == 'bot testing' || ctx.chat.title == 'Accelerator Group 3') {
+    if (ctx.chat.title == 'bot testing') {
         var messageAuthorName = ctx.from.first_name
         if (ctx.from.last_name) {
             messageAuthorName += ' ' + ctx.from.last_name
@@ -61,18 +61,37 @@ discordBot.on('ready', () => {
 })
 
 discordBot.on('messageCreate', (msg) => {
-    if (msg.channel.id != '917684042797817887' && msg.channel.id != '917660123198029875') return
+    if (msg.channel.id != '917660123198029875') return
     if (msg.author.bot) return
     var messageText = msg.content
     var authorName = msg.guild.members.cache.get(msg.author.id).nickname
-    
     telegramBot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, `From ${authorName}: ${messageText}`)
-    .then(msg => {
+    .then(message => {
+        if (msg.attachments.size > 0) {
+            try {
+                msg.attachments.forEach(attachment => {
+                    console.log(attachment.contentType)
+                    if (attachment.contentType.startsWith('video')) {
+                        telegramBot.telegram.sendVideo(process.env.TELEGRAM_CHAT_ID, attachment.attachment)
+                    } else if (attachment.contentType.startsWith('image')) {
+                        telegramBot.telegram.sendPhoto(process.env.TELEGRAM_CHAT_ID, attachment.attachment)
+                    } else if (attachment.contentType.startsWith('audio')) {
+                        telegramBot.telegram.sendAudio(process.env.TELEGRAM_CHAT_ID, attachment.attachment)
+                    } else {
+                        telegramBot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, attachment.url)
+                    }
+                })
+            } catch(err) {
+                console.log('Error in sending Discord attachments: ' + err)
+            }
+        }
+
         console.log(`Message from ${authorName} streamed. <D/T>`)
     })
 })
 
 telegramBot.launch()
 console.log('Bot is online and ready to stream!')
+console.log('Connecting to Discord servers...')
 
 discordBot.login(process.env.DISCORD_BOT_TOKEN)
